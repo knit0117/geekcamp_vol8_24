c017d45099edf351f260b34e57d4fb1a26c30230
knit0117 <s18t313@kagawa-u.ac.jp>
2023-08-20 02:47:51 +0000

    fix

--- selectstr.js
+++ selectstr.js
@@ -1,4 +1,4 @@
-chrome.extension.onRequest.addListener(() => {
+chrome.extension.onMessage.addListener(() => {
     const selection = window.getSelection()
     const text = selection.toString()
     console.log(text)
